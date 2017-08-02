396b142ed8491a593d83a21ab3cca2538350a2c0
jeyfost <foster_andrew@tut.by>
2017-08-02 15:06:32 +0300

    Добавлена кнопка-ссылка на страницу отзывов

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by jeyfost on 02.08.2017.
  */
+
+function reviewsButton(action) {
+	if(action === 1) {
+		document.getElementById('allReviewsButton').style.backgroundColor = "#e3e3e3";
+		document.getElementById('allReviewsButton').style.color = "#4651ed";
+	} else {
+		document.getElementById('allReviewsButton').style.backgroundColor = "#457bdf";
+		document.getElementById('allReviewsButton').style.color = "#fff";
+	}
+}
