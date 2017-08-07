dc1b9a087c26613e5b2b6f5983994c26ada05325
jeyfost <foster_andrew@tut.by>
2017-08-07 18:53:35 +0300

    Добавлено перенаправление из категорий в первые по приоритету галереи

---
+++ wedding/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeyfost
+ * Date: 07.08.2017
+ * Time: 17:45
+ */
+
+include("../scripts/connect.php");
+
+$categoryResult = $mysqli->query("SELECT * FROM categories WHERE sef_link = 'wedding'");
+$category = $categoryResult->fetch_assoc();
+
+if(!empty($category)) {
+	$subcategoryResult = $mysqli->query("SELECT * FROM subcategories WHERE category_id = '".$category['id']."' ORDER BY priority LIMIT 1");
+	$subcategory = $subcategoryResult->fetch_assoc();
+
+	if(!empty($subcategory)) {
+		header("Location: ../".$subcategory['sef_link']);
+	} else {
+		header("Location: ../");
+	}
+} else {
+	header("Location: ../");
+}
