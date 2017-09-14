bbba787f6634112dd2a014fb8c265b9712bcb799
jeyfost <foster_andrew@tut.by>
2017-09-14 15:24:01 +0300

    Исправлены слайдеры для мобильных устройств

--- js/main-slider.js
+++ js/main-slider.js
@@ -2,13 +2,25 @@
  * Created by jeyfost on 31.07.2017.
  */
 
-$(window).on("load", function() {
-	$('#slider').height(parseInt($(window).height() - $('#slider').offset().top));
-});
-
 var slides = document.querySelectorAll('#slider .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 3500);
+
+$(window).on("load", function() {
+	$('#slider').height(parseInt($(window).height() - $('#slider').offset().top));
+
+	if($(document).width() < 768) {
+		var minHeight = slides[0].firstChild.height;
+
+		for(var i = 1; i < slides.length; i++) {
+			if(slides[i].firstChild.height < minHeight) {
+				minHeight = slides[i].firstChild.height;
+			}
+		}
+
+		$('#slider').height(minHeight);
+	}
+});
 
 function nextSlide() {
 	if(slides[currentSlide].firstChild.height < $('#slider').height()) {
